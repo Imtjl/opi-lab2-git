ee589e2111ed8819015c2554e522a274ebda32d0
MomsDeveloper <georgiy.khabner@mail.ru>
2024-03-16 00:11:43 +0300

    r0

---
+++ E.java
@@ -0,0 +1,30 @@
+public class E implements K, B {
+
+    private String c = "init";
+
+    private String d = "init";
+
+    public java.lang.Class qq() {
+        return getClass();
+    }
+
+    public void bb() {
+        System.out.println(42);
+    }
+
+    public Object rr() {
+        return null;
+    }
+
+    public double ad() {
+        return 12.12;
+    }
+
+    public java.util.List<String> jj() {
+        return new java.util.LinkedList<String>();
+    }
+
+    public long dd() {
+        return 33;
+    }
+}
---
+++ J.java
@@ -0,0 +1,14 @@
+public class J extends E {
+
+    private int f = 1;
+
+    private double d = 100.500;
+
+    public int af() {
+        return -1;
+    }
+
+    public int cc() {
+        return 42;
+    }
+}
